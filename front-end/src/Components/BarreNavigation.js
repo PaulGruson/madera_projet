adae401d28ab31b40681be7fccff73c82d60101a
Paul <paul.gruson@orange.fr>
2021-02-15 12:44:51 +0100

    barre de nav

--- front-end/src/Components/BarreNavigation.js
+++ front-end/src/Components/BarreNavigation.js
@@ -3,9 +3,12 @@ import React from 'react'
 
 function BarreNavigation() {
     return (
-        <div>
-            <p> fsdgdsg </p>
-        </div>
+        <ul>
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
     )
 }
 
