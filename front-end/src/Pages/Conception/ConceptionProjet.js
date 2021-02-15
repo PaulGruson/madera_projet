8f8051148ec2506961a73d9faa05fcd4bdb014b6
maxime <bps_maxime@outlook.fr>
2021-02-15 13:58:19 +0100

    conception projet
    Ajout de l'html
    reste : css, handlechange, requete pour créer projet

--- front-end/src/Pages/Conception/ConceptionProjet.js
+++ front-end/src/Pages/Conception/ConceptionProjet.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BarreNavigation } from '../../Components/BarreNavigation'
 
 function ConceptionProjet(props) {
 
@@ -7,10 +8,55 @@ function ConceptionProjet(props) {
     } = props
 
     return (
-        <div>
-         <p>Conception projet</p>   
-        </div>
+        <body>
+            < BarreNavigation />
+            <section id="conceptionProjet" className="conception">
+                <h1>Conception de devis</h1>
+                <div classname="conception-box">
+                    <h2>Projet</h2>
+                    <div>
+                        <label for="nomDuProjet">Nom du projet</label>
+                        <input id="nomDuProjet" type="text" name="nomDuProjet" />
+                    </div>
+                    <div>
+                        <label for="nomDuProjet">Référence du projet</label>
+                        <input id="nomDuProjet" type="text" name="nomDuProjet" />
+                    </div>
+                    <div>
+                        <label for="nomDuProjet">Date</label>
+                        <input id="nomDuProjet" type="date" name="nomDuProjet" />
+                    </div>
+                    <div>
+                        <label for="gamme">Gamme</label>
+                        <select id="gamme" name="game" >
+                            <option value="gamme1">Gamme1</option>
+                            <option value="gamme2">Gamme2</option>
+                            <option value="gamme3">Gamme3</option>
+                        </select>
+                    </div>
+                </div>
+                <div classname="conception-box">
+                    <h2>Client</h2>
+                    <div>
+                        <label for="nomDuClieny">Nom du client</label>
+                        <input id="nomDuClient" type="text" name="nomDuClient" />
+                    </div>
+                    <div>
+                        <label for="prenomDuClient">Prénom du client</label>
+                        <input id="prenomDuClient" type="text" name="prenomDuClient" />
+                    </div>
+                    <div>
+                        <label for="adresseDuClient">Adrresse du client</label>
+                        <input id="adresseDuClient" type="text" name="adresseDuClient" />
+                    </div>
+                    <div>
+                        <label for="mailDuClient">Mail du client</label>
+                        <input id="mailDuClient" type="text" name="mailDuClient" />
+                    </div>
+                </div>
+            </section>
+        </body>
     )
 }
 
-export { ConceptionProjet }
+export { ConceptionProjet }
